2d1eec65fc4f1d0f7482bec079ad2363448d9790
syamsul muttaqin <lombok.oc@gmail.com>
2016-08-05 07:17:57 +0800

    fixing loading service account not found when using @reboot due to relative path problems

--- main.js
+++ main.js
@@ -3,14 +3,33 @@ var Firebase = require('firebase'),
 	FirebaseHandler = require('./app/firebase-handler'),
 	GPSParser = require('./app/gps-parser'),
 	gps = new GPSParser('/dev/ttyUSB0', 9600),
+	path = require('path'),
 	// gpio = require('rpi-gpio'),
 	gpio = require('./app/rpi-gpio-dummy'),
 	_ = require('lodash'),
 	argv = process.argv,
 	GPIOPins = [7,11,12,13,15,16,18,22,29,31,32,33,35,36,37,38,40];
 
+// current directory where this app is located
+var currentDir = path.dirname(__dirname);
+
+/*
+In case there is space to the path to the current directory 
+we need to normalize it, so, node still can find the service account file
+when we initialize the firebase
+*/
+var arrayOfPath = currentDir.split(' ');
+
+arrayOfPath.forEach((item, i) => {
+	if ( i < arrayOfPath.length - 1 ){
+		arrayOfPath[i] += "\\";
+	}
+});
+
+var finalPath = arrayOfPath.join(' ');
+
 Firebase.initializeApp({
-    serviceAccount:'app/service_account.json',
+    serviceAccount:finalPath + '/app/service_account.json',
     databaseURL:'https://vehicle-iot.firebaseio.com'
 });
 

bd97c0502958e395b2bda068ce2c97cb9f7fa605
syamsul muttaqin <lombok.oc@gmail.com>
2016-08-11 10:58:17 +0800

    fixing bug on finding path to service_account.json

--- main.js
+++ main.js
@@ -4,38 +4,24 @@ var Firebase = require('firebase'),
 	GPSParser = require('./app/gps-parser'),
 	gps = new GPSParser('/dev/ttyUSB0', 9600),
 	path = require('path'),
-	gpio = require('rpi-gpio'),
-	//gpio = require('./app/rpi-gpio-dummy'),
+	// gpio = require('rpi-gpio'),
+	gpio = require('./app/rpi-gpio-dummy'),
 	_ = require('lodash'),
 	argv = process.argv,
 	GPIOPins = [7,11,12,13,15,16,18,22,29,31,32,33,35,36,37,38,40];
 
 var VEHICLE_ID = 'DR3559KE';
-// current directory where this app is located
-var currentDir = `${__dirname}`;
-/*
-In case there is space to the path to the current directory 
-we need to normalize it, so, node still can find the service account file
-when we initialize the firebase
-*/
-var arrayOfPath = currentDir.split(' ');
 
-arrayOfPath.forEach((item, i) => {
-	if ( i < arrayOfPath.length - 1 ){
-		arrayOfPath[i] += "\\";
-	}
-});
-
-var finalPath = arrayOfPath.join(' ');
+var appRootPath = `${path.dirname(__dirname)}/${path.basename(__dirname)}`;
 
 Firebase.initializeApp({
-    serviceAccount:finalPath + '/app/service_account.json',
+    serviceAccount:`${appRootPath}/app/service_account.json`,
     databaseURL:'https://vehicle-iot.firebaseio.com'
 });
 
-var dbRef = Firebase.database().ref();
+var dbRef = Firebase.database().ref(VEHICLE_ID);
 var powerPin = (argv[2] === '--pin' || argv[2] === '-p')  && !isNaN(parseInt(argv[3])) && _.includes(GPIOPins, parseInt(argv[3])) ? argv[3] : 11;
-var Handler = new FirebaseHandler(dbRef, gpio, powerPin);
+var Handler = new FirebaseHandler(dbRef, gpio, powerPin, VEHICLE_ID);
 
 Handler.watchCommand();
 Handler.ready(() => {
